8225095f172dd3a859816dcd5623fca44e4e4751
charan <acharan.xyz@gmail.com>
2020-11-29 00:02:15 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormatListBulletedRoundedIcon from '@material-ui/icons/FormatListBulletedRounded';
+
+function Header() {
+  return (
+    <header>
+      <h1><FormatListBulletedRoundedIcon /> Keeper </h1>
+    </header>
+  );
+}
+
+export default Header;
